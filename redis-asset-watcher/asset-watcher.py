fb7ac5c2c267c86ff893096193b7b4d23f9fcaeb
Charlie Federspiel <charlie.federspiel@epsilon.com>
2025-07-19 13:47:47 -0700

    event-based scheduling is only available for SQS atm.

--- redis-asset-watcher/asset-watcher.py
+++ redis-asset-watcher/asset-watcher.py
@@ -1,21 +1,24 @@
 from airflow.models.dag import DAG
 from airflow.sdk import Asset
+# from airflow.providers.redis import RedisMessageQueueTrigger
 from airflow.providers.common.messaging.triggers.msg_queue import MessageQueueTrigger
 from airflow.sdk import AssetWatcher
 from airflow.decorators import task
 import pendulum
 
+queueWatcher = Asset("review_queue_asset", watchers=[
+        AssetWatcher(
+            name="demo-redis-watcher", 
+            trigger=MessageQueueTrigger( 
+                queue="redis://SC1WASPNAPP03.res.prod.global:6383/CJF_REVIEW_QUEUE"
+            )
+        )
+    ]);
+
 with DAG(
     dag_id="example_event_driven_pipeline",
     start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
-    schedule=Asset("review_queue_asset", watchers=[
-        AssetWatcher(
-            name="demo-redis-watcher", 
-            trigger=MessageQueueTrigger(
-                queue="redis://redis/review_queue"
-            )
-        )
-    ]),
+    schedule=[queueWatcher],
     catchup=False,
 ) as dag:
     @task
